6a0cab3197178c426eb1d4850dec2e89cf7a8154
Marvin Schmitt <mail@marvinschmitt.com>
2022-02-11 14:17:18 +0100

    add col_scale to stat_simplex_density (controls color scale mapping, e.g. sqrt or log)

--- R/simplex_density_utils.R
+++ R/simplex_density_utils.R
@@ -5,19 +5,33 @@
 #'
 #' @return A matrix of color codes
 #' @export
-construct_density_matrix <- function(density_function, args){
+construct_density_matrix <- function(density_function, args, 
+                                     resolution = 101,
+                                     col_scale="linear"){
   B = as.matrix(data.frame(x = c(0, 1, 0.5), y=c(0, 0, sqrt(3)/2)))
   
-  resolution = 101
-  
   n_colors = 1000
-  viridis_palette = viridis::viridis(n=n_colors)
+  color_palette = viridis::viridis(n=n_colors)
   
   # Nyquist Sampling Theorem -> sample at >2*freq_max
   pi_grid = construct_simplex_grid(n_x = 2.01*resolution, n_y = 2.01*resolution)
   args = append(args, list(x=pi_grid), after=0)
   pi_grid_density = do.call(density_function, args)
-  pi_grid_density = pi_grid_density^(1/4)
+  if (is.null(col_scale) || col_scale=="linear"){
+    
+  }
+  else if (col_scale == "log"){
+    pi_grid_density = log(pi_grid_density)
+  }
+  else if (col_scale == "sqrt"){
+    pi_grid_density = sqrt(pi_grid_density)
+  }
+  else if (is.numeric(col_scale)){
+    pi_grid_density = pi_grid_density ^ (1/col_scale)
+  }
+  else{
+    stop("Please provide a valid mapping or leave empty!")
+  }
   
   cuts = seq(
     min(pi_grid_density), 
@@ -33,7 +47,7 @@ construct_density_matrix <- function(density_function, args){
                            }
   )) # calculate color indices (as array)
   
-  colors = viridis_palette[color_indices]
+  colors = color_palette[color_indices]
   
   M = matrix(data = NA, nrow = resolution, ncol=resolution)
   
--- R/stat_simplex_density.R
+++ R/stat_simplex_density.R
@@ -11,6 +11,9 @@
 #' @param inherit.aes 
 #' @param fun 
 #' @param args 
+#' @param col_scale specify a color scale mapping. 
+#' Possible values include: "linear", "sqrt", "log", any numeric n for n'th root, 
+#' NULL (defaults to linear)
 #' @param ... 
 #'
 #' @return
@@ -19,7 +22,7 @@ stat_simplex_density <- function(mapping = NULL, data = NULL,
                                  geom = "simplex_heatmap",
                                  position = "identity", na.rm = FALSE, show.legend = NA, 
                                  inherit.aes = FALSE, fun = NULL, 
-                                 args = NULL, 
+                                 args = NULL, col_scale = "linear",
                                  ...) {
   mapping = ggplot2::aes_all(args)
   
@@ -27,20 +30,23 @@ stat_simplex_density <- function(mapping = NULL, data = NULL,
   ggplot2::layer(
     stat = StatSimplexDensity, data = data, mapping = mapping, geom = geom, 
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(fun = fun, args = args, na.rm = na.rm,...)
+    params = list(fun = fun, args = args, col_scale = col_scale,
+                  na.rm = na.rm,...)
   )
 }
 
 # ggproto ----------
 StatSimplexDensity <- ggplot2::ggproto(
   "StatSimplexDensity", ggplot2::Stat, 
-  compute_group = function(data, scales, fun=NULL, args=NULL) {
+  compute_group = function(data, scales, 
+                           fun=NULL, args=NULL, col_scale="linear") {
     density_args = lapply(args, function(arg) data[, as.character(arg)][[1]])
-    
     # TODO: Create a lookup for common name conflicts
     names(density_args)[names(density_args) == "Alpha"] = "alpha"
     
-    M = construct_density_matrix(fun, density_args)
+    M = construct_density_matrix(density_function = fun, 
+                                 args = density_args, 
+                                 col_scale = col_scale)
     d = data.frame(0)
     d$M = list(M)
     return(d)
